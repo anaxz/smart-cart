4035a9957b9fe5fe1479e5fa0036f5b1d13f14aa
Anna <anaxz@users.noreply.github.com>
2022-12-22 14:59:11 +0000

    refactored files; added link to react to flask

--- api/app.py
+++ api/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/dist', static_url_path='/')
 CORS(app)
 
 @app.route('/')
 def default():
-    return 'Welcome'
+    return 'Welcome back'
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
