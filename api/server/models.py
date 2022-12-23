cf9306aa6be197d1a6f5b5052a8da8e1e24afba7
Anna <anaxz@users.noreply.github.com>
2022-12-23 12:28:43 +0000

    add missing booleans

--- api/server/models.py
+++ api/server/models.py
@@ -3,6 +3,9 @@ from . import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        return f"User {self.name}, {self.email}"
