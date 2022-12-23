fcc1aeec67e707f04f30ce0719bd1a0c24046e76
“abdul-san” <kamara847@gmail.com>
2022-12-23 12:12:56 +0000

    added login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import './assets/App.css'
-import { Welcome } from './components'
+import "./assets/index.css"
+import { Welcome } from './pages'
+import {Navbar} from "./components"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Darknavbar } from './layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from './components/login';
 
 function App() {
 
   return (
+    <>
+    <Darknavbar className="sticky-top"/>
     <div className="App">
       <Welcome />
     </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
