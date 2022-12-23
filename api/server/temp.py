be47924aba2da24f8005718405444fd872f53024
bradgichangi <bradgichangi@gmail.com>
2022-12-23 14:44:12 +0000

    Added SQL queries

--- api/server/temp.py
+++ api/server/temp.py
@@ -8,6 +8,15 @@ conn = psycopg2.connect(url)
 cur = conn.cursor()
 search = ' SELECT * FROM Users; '
 
+def get_list(id):
+    query = f"SELECT * FROM Shopping_List WHERE list_id = {id}"
+
+def get_all_lists(id):
+    query = f"SELECT product_id FROM Shopping_List JOIN Users_Shopping ON Shopping_List.list_id = Users_Shopping.id WHERE Users_Shopping.user_id = {id}"
+
+def get_favourites(id):
+    query = f"SELECT * FROM Favourites WHERE user_id = {id}'"
+
 cur.execute(search)
 res = cur.fetchall()
 print(res)
