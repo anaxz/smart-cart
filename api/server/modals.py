1142d0a6f7daf201210495efd5ad215a660fe4cb
Anna <anaxz@users.noreply.github.com>
2022-12-23 09:48:48 +0000

    started modal file

---
+++ api/server/modals.py
@@ -0,0 +1,7 @@
+from flask_login import UserMixin
+from . import db 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
