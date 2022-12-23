35db4d7841c626d27fcc470089d9887dbe46e647
bradgichangi <bradgichangi@gmail.com>
2022-12-23 14:29:20 +0000

    Database connected

--- api/server/temp.py
+++ api/server/temp.py
@@ -1,12 +1,12 @@
 import os
 import psycopg2
 
-url = "postgres://test_2a4o_user:p0SqIlVeHJtjrDUdqWR0fKe6RhTbHRtN@dpg-ceip6ckgqg4dlfcjigd0-a.frankfurt-postgres.render.com/test_2a4o"
+url = "postgres://admin:x3tGfygzNAP2JEOKGSHUMl1LOxQb4Ig9@dpg-ceirh32rrk0a7i0av6q0-a.frankfurt-postgres.render.com/smart_cart_kjou"
 
 conn = psycopg2.connect(url)
 
 cur = conn.cursor()
-search = ' SELECT * FROM History; '
+search = ' SELECT * FROM Users; '
 
 cur.execute(search)
 res = cur.fetchall()
