2563c01b72f20f8af0efb646b9b37d3e3a162c36
bradgichangi <bradgichangi@gmail.com>
2022-12-23 14:49:06 +0000

    Updated SQL queries

--- api/server/temp.py
+++ api/server/temp.py
@@ -8,15 +8,31 @@ conn = psycopg2.connect(url)
 cur = conn.cursor()
 search = ' SELECT * FROM Users; '
 
+def get_user(id):
+    query = f"SELECT * FROM Users WHERE id = {id}"
+    cur.execute(query)
+    res = cur.fetchall()
+    print(res)
+
 def get_list(id):
     query = f"SELECT * FROM Shopping_List WHERE list_id = {id}"
+    cur.execute(query)
+    res = cur.fetchall()
+    print(res)
 
 def get_all_lists(id):
     query = f"SELECT product_id FROM Shopping_List JOIN Users_Shopping ON Shopping_List.list_id = Users_Shopping.id WHERE Users_Shopping.user_id = {id}"
+    cur.execute(query)
+    res = cur.fetchall()
+    print(res)
 
 def get_favourites(id):
-    query = f"SELECT * FROM Favourites WHERE user_id = {id}'"
+    query = f"SELECT * FROM Favourites WHERE user_id = {id}"
+    cur.execute(query)
+    res = cur.fetchall()
+    print(res)
 
-cur.execute(search)
-res = cur.fetchall()
-print(res)
+get_user(1)
+get_favourites(1)
+get_list(1)
+get_all_lists(1)
