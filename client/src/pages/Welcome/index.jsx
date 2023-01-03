c1b9c2616ae639fd737c9b8b5daff00a2c4a49de
“abdul-san” <kamara847@gmail.com>
2023-01-03 11:49:31 +0000

    Edited welcome page and added link to home page

--- client/src/pages/Welcome/index.jsx
+++ client/src/pages/Welcome/index.jsx
@@ -4,17 +4,17 @@ import Button from 'react-bootstrap/Button';
 const Welcome = () => {
     return (
         <>
-      <div className='text-center'>
-      <div>
-        <h1>Welcome to Smart-Cart</h1>
-        <h3>Your one-stop smart shopping resource</h3>
-      </div>
-      <div className="mb-2">
-        <Button variant="primary" size="lg" href="#">Get Started</Button>
-      </div>
-      </div>
-      </>
+            <div className="pt-5 d-grid gap-3 ">
+                <div className="pt-5 text-center">
+                    <p className="fs-1">Welcome to Smart-Cart</p>
+                    <p className="fs-2">Your one-stop smart shopping resource</p>
+                </div>
+                <div className="p-2 fs-2 text-center">
+                    <Button variant="primary" size="lg" href="/home">Get Started</Button>
+                </div>
+            </div>
+        </>
     );
-  };
+};
 
 export default Welcome;
