ec1b1c7fd88efdb1e3c26e053fef6496ee7f3dc1
“abdul-san” <kamara847@gmail.com>
2023-01-03 10:41:24 +0000

    Added button to homepage

--- client/src/pages/Welcome/index.jsx
+++ client/src/pages/Welcome/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-const Welcome = () => <h1>Welcome</h1>
+const Welcome = () => {
+    return (
+        <>
+      <div className='text-center'>
+      <div>
+        <h1>Welcome to Smart-Cart</h1>
+        <h3>Your one-stop smart shopping resource</h3>
+      </div>
+      <div className="mb-2">
+        <Button variant="primary" size="lg" href="#">Get Started</Button>
+      </div>
+      </div>
+      </>
+    );
+  };
 
 export default Welcome;
