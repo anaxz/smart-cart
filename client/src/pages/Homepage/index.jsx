65a4c3a68935f82455c18c40edef5a432c835f9c
Arqestra <mshawdietrich@gmail.com>
2023-01-03 18:30:26 +0000

    Added a modal and cart button

--- client/src/pages/Homepage/index.jsx
+++ client/src/pages/Homepage/index.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Foodbar } from "../../components";
 import { Darknavbar } from "../../layout";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './index.css'
 
 const Homepage = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
-        <Darknavbar />
-        <Foodbar />
+            <Darknavbar />
+            <Button id="basket" variant="danger" onClick={handleShow}><i class="bi bi-basket3"></i></Button>
+            <Foodbar />
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Your Comparison Cart</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Compare Prices!
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
