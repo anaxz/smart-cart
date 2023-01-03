97edf0a7ad71e6c35683f91c52fe679d2ba9220b
Arqestra <mshawdietrich@gmail.com>
2023-01-03 10:57:42 +0000

    Added example cards and formatting changes

--- client/src/layout/Darknavbar/index.jsx
+++ client/src/layout/Darknavbar/index.jsx
@@ -12,29 +12,29 @@ function Darknavbar() {
     <>
       <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Smart-Cart</Navbar.Brand>
+          <Navbar.Brand href="#home">Smart-Cart <i class="bi bi-cart2"></i></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#profile">Profile</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/Auth">Login</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#profile">Profile</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/Auth">Login</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <br />
-      </>
+    </>
   )
 }
 
