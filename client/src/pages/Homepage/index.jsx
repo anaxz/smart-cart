80abf6046a26cd1a65bebf22a6c528b329fdc9d0
Arqestra <mshawdietrich@gmail.com>
2023-01-04 20:08:56 +0000

    Made modal a component

--- client/src/pages/Homepage/index.jsx
+++ client/src/pages/Homepage/index.jsx
@@ -9,45 +9,16 @@ import { deleteItem } from '../../reducer'
 
 import './index.css'
 
-const Homepage = (props) => {
+const Homepage = () => {
 
-    const [show, setShow] = useState(false);
-    const [showSearch, setShowSearch] = useState(false);
-    const [searchItem, setSearchItem] = useState('')
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-
-    let items = useSelector(state => state).arr
-    console.log('Redux')
-    console.log(items)
 
     return (
         <>
             <Darknavbar />
-            <Button id="basket" variant="danger" onClick={handleShow} style={{ position: 'absolute', right: '230px', top: '112px', height: '56px' }}><i className="bi bi-cart"></i> Cart</Button>
+
             <Foodbar />
-            <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Your Smart-Cart</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <ul id="cartitems">
-                        {items.map(item => <><li>{item}</li><button onClick={() => dispatch(deleteItem(item))}>Remove</button></>)}
-                    </ul>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={() => navigate('/comparison')}>
-                        Compare Prices!
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+
         </>
     )
 }
